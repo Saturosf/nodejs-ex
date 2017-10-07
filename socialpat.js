4c8707783fe7b4178188bd14d945c90d0b8aa32c
Saturosf <32597565+Saturosf@users.noreply.github.com>
2017-10-07 17:48:52 +0200

    Add files via upload

---
+++ socialpat.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var port = 3000;
+
+http.createServer(function (req,res){
+	res.writeHead(200,{'Content-type':'text/html'});
+	res.write('<h1>Hello world</h1>');
+	res.end();
+	
+	
+}).listen(port);
+
+console.log('El servidor esta corriendo en el puerto '+port);
